5f3bc7aa42b20c732e0792ab767222a69034aa88
sai-myne <ahlusir@gmail.com>
2020-09-01 10:57:47 +0800

    Tween Sequence

--- lib/shared/heart.dart
+++ lib/shared/heart.dart
@@ -8,20 +8,24 @@ class Heart extends StatefulWidget {
 class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
   bool isFav = false;
   AnimationController _controller;
-  Animation _colorAnimation;
+  Animation<Color> _colorAnimation;
+  Animation<double> _sizeAnimation;
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     _controller =
-        AnimationController(duration: Duration(milliseconds: 500), vsync: this);
+        AnimationController(duration: Duration(milliseconds: 300), vsync: this);
     _colorAnimation = ColorTween(begin: Colors.grey[400], end: Colors.red)
         .animate(_controller);
 
-    _controller.addListener(() {
-      print(_controller.value);
-      print(_colorAnimation.value);
-    });
+    _sizeAnimation = TweenSequence(<TweenSequenceItem<double>>[
+      TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 30, end: 50), weight: 50),
+      TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 50, end: 30), weight: 50),
+    ]).animate(_controller);
 
     _controller.addStatusListener((status) {
       if (status == AnimationStatus.completed) {
@@ -53,7 +57,7 @@ class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
           icon: Icon(
             Icons.favorite,
             color: _colorAnimation.value,
-            size: 30,
+            size: _sizeAnimation.value,
           ),
           onPressed: () {
             isFav ? _controller.reverse() : _controller.forward();
